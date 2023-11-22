f4ad6437a3c3ac85ddfc061ef2176398ccccec3c
ad113 <adrien.joliat@bluewin.ch>
2023-11-22 16:16:26 +0100

    cleaning and working

--- AIA_requests.py
+++ AIA_requests.py
@@ -20,18 +20,18 @@ import torch
 
 # Load the csv data containing the events
 data = load_data()
-
+num_images=30
 # Get the number of event and initialize the output array
-events_list = [data[data.index==i] for i in range(1,3)] # Will become the full list of event when we actually download everything
+events_list = [data[data.index==i] for i in range(0,5)] # Will become the full list of event when we actually download everything
 #N_events = data.shape[0] : for when we download everything
 N_events = len(events_list)
-data_array = np.zeros((N_events, 500, 500, 11))   
+data_array = np.zeros((N_events, 500, 500, num_images))   
 # last dim is the number of images per sequence => needs to be consistent with the end_time and sample time in the get_image function
 
 
 # This loop goes over each data line of the .csv file
 for i, event in enumerate(events_list):
-    files = get_image(event) # selects data line i
+    files = get_image(event, num_images) # selects data line i
     # "files" is (class <parfive>) and contains N <HDUList> objects (where N is the nb of images in the sequence) 
     # that we open as "f". The attribute f.data returns a numpy array in our case, bc the data is an image.
     # sequence_array is a 3D array of shape (500, 500, N) which contains all the pixel values for one line of data (~ 1 event)
@@ -41,12 +41,12 @@ for i, event in enumerate(events_list):
     data_array[i,:,:,:] = sequence_array
 
     # Plotting the numpy arrays to check that we get the images => need improvement to chech the image's quality
-
+"""
     for j in range(len(files)):
         img = sequence_array[:,:,j]
         plt.imshow(img, cmap='hot', interpolation='nearest')
         plt.show()
-
+"""
 # Check the size of final array
 print(data_array.shape)
 
@@ -54,3 +54,4 @@ print(data_array.shape)
 # Find a way to export the array to an external file the reuse in the model. Possibilities to look at:
 # .npy format and np.save function
 # HPF5 format (good with large amount of data)
+np.save("data_1.npy",data_array)
