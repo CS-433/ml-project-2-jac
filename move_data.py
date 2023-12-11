10895df1d0f2eb7d65cfec0e3b9113dbadd798b5
chjulie <julie.charlet@epfl.ch>
2023-12-11 19:02:58 +0100

    now val/test/train folder + tool to visualize transforms in Adrien_RCNN.ipynb

--- move_data.py
+++ move_data.py
@@ -22,25 +22,48 @@ def move_random_files(source_folder, destination_folder, percentage):
 # Specify the source and destination folders
 source_1 = "./data1"
 destination_1 = "./data1_test"
+destination_1_val = "./data1_val"
 
 source_0 = "./data0"
 destination_0 = "./data0_test"
+destination_0_val = "./data0_val"
 
 # Specify the percentage of files to move (20%)
-percentage_to_move = 0.2
+percentage_to_move = 0.15
 
 # Call the function to move random files
-move_random_files(source_1, destination_1, percentage_to_move)
-move_random_files(source_0, destination_0, percentage_to_move)
+move_random_files(source_1, destination_1, 0.15)
+move_random_files(source_1, destination_1_val, 0.175)
 
+move_random_files(source_0, destination_0, 0.15)
+move_random_files(source_0, destination_0_val, 0.175)
+
+
+# moving back to original folder
+"""
+move_random_files(destination_1, source_1, 1)
+move_random_files(destination_0, source_0, 1)
+move_random_files(destination_1_val, source_1, 1)
+move_random_files(destination_0_val, source_0, 1)
+"""
+
+# Final check
 files_test1 = os.listdir("./data1_test")
+files_val1 = os.listdir("./data1_val")
 files_1 = os.listdir("./data1")
+
 files_test0 = os.listdir("./data0_test")
+files_val0 = os.listdir("./data0_val")
 files_0 = os.listdir("./data0")
 
 print(" # of files in data1_test", len(files_test1))
+print(" # of files in data1_val", len(files_val1))
 print(" # of files in data1", len(files_1))
+
 print(" # of files in data0_test", len(files_test0))
+print(" # of files in data0_val", len(files_val0))
 print(" # of files in data0", len(files_0))
+
+
 
 
